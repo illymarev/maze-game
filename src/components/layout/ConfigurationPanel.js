619b3f1d5d7e0528339dc923809b1f76a55700c3
illymarev <illymarev@gmail.com>
2023-02-08 22:15:23 -0500

    Refactored the configuration panel to use mui

--- src/components/layout/ConfigurationPanel.js
+++ src/components/layout/ConfigurationPanel.js
@@ -1,54 +1,55 @@
-import Button from '@mui/material/Button';
-import {Box, Container, Stack, Grid} from "@mui/material";
+import {Button, InputLabel, MenuItem, Slider, Stack, Select} from "@mui/material";
 
 
 const ConfigurationPanel = () => (
-    <Stack spacing={3} sx={{
-        'align-items': 'center',
-        'margin-top': '1rem'
-    }}>
-        <Stack direction='row' spacing={5}>
-            <Button variant="contained" color='secondary' size='large'
-                    sx={{
-                        width: '200px',
-                        height: '50px',
-                        'border-radius': '1.25rem',
-                    }}>Generate</Button>
-            <Button variant="contained" color='primary' size='large'
-                    sx={{
-                        width: '200px',
-                        height: '50px',
-                        'border-radius': '1.25rem',
-                    }}>Solve</Button>
-        </Stack>
-        <Stack sx={{
-            border: '1px solid #ccc',
-            'border-radius': '1.25rem',
-            padding: '1rem',
-        }}>
-            <Stack direction='row' spacing={2}>
-                <div className='label_and_input'>
-                    <label> Generation Algorithm
-                        <select name='generation_algorithm_selector' id='generation_algorithm_selector'>
-                            <option value='recursive_backtracking' selected='selected'>Recursive Backtracking</option>
-                            <option value='random'>TODO</option>
-                        </select>
-                    </label>
-                </div>
-                <div className='label_and_input'>
-                    <label> Solving Algorithm
-                        <select name='solving_algorithm_selector' id='solving_algorithm_selector'>
-                            <option value='dijkstras_algorithm' selected='selected'>Dijkstra's Algorithm</option>
-                            <option value='random'>TODO</option>
-                        </select>
-                    </label>
-                </div>
+    <Stack spacing={3} alignItems='center' marginTop={'1rem'}>
+        <Stack alignItems='center' direction='row' spacing={2}>
+            <Stack width={'250px'}>
+                <InputLabel id="generation_algorithm_label">Generation Algorithm</InputLabel>
+                <Select
+                    labelId="generation_algorithm_label"
+                    id="generation_algorithm_selector"
+                    sx={{'border-radius': '1.25rem'}}
+                    value='recursive_backtracking'>
+                    <MenuItem value="recursive_backtracking">Recursive Backtracking</MenuItem>
+                    <MenuItem value="random">TODO</MenuItem>
+                </Select>
             </Stack>
-            <div className='label_and_input'>
-                <label>Visualization Speed
-                    <input type='range' name='visualization_speed' id='visualization_speed' min='1' max='100'/>
-                </label>
-            </div>
+            <Stack spacing={2}>
+                <Button variant="contained" color='secondary' size='large'
+                        sx={{
+                            width: '200px',
+                            height: '50px',
+                            'border-radius': '1.25rem',
+                        }}>Generate</Button>
+                <Button variant="contained" color='primary' size='large'
+                        sx={{
+                            width: '200px',
+                            height: '50px',
+                            'border-radius': '1.25rem',
+                        }}>Solve</Button>
+            </Stack>
+            <Stack width={'250px'}>
+                <InputLabel id="solving_algorithm_label">Solving Algorithm</InputLabel>
+                <Select
+                    labelId="solving_algorithm_label"
+                    id="solving_algorithm_selector"
+                    sx={{'border-radius': '1.25rem'}}
+                    value='dijkstras_algorithm'>
+                    <MenuItem value="dijkstras_algorithm">Dijkstra's Algorithm</MenuItem>
+                    <MenuItem value="random">TODO</MenuItem>
+                </Select>
+            </Stack>
+        </Stack>
+        <Stack direction='column' justifyContent='center' alignItems='center'>
+            <InputLabel id="visualization_speed_label">Visualization Speed</InputLabel>
+            <Slider
+                labelId="visualization_speed_label"
+                id='visualization_speed_slider'
+                defaultValue={50}
+                valueLabelDisplay="auto"
+                size='medium'
+                sx={{width: '730px'}}/>
         </Stack>
     </Stack>
 )
