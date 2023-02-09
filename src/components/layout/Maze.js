1ebb83b0e6ad41f2642c21870887d6d8bdb86e06
illymarev <illymarev@gmail.com>
2023-02-08 23:10:49 -0500

    Started working on the maze

--- src/components/layout/Maze.js
+++ src/components/layout/Maze.js
@@ -1,17 +1,28 @@
-const Maze = () => (
-    <div className="maze">
-        <div className='maze_legend'>
-            <ul>
-                <li>Visited</li>
-                <li>Wall</li>
-                <li>Start</li>
-                <li>Finish</li>
-            </ul>
-        </div>
-        <div className='maze_grid'>
-            {/*TODO later*/}
-        </div>
-    </div>
-)
+import {Grid, Stack, Box} from "@mui/material";
+
+const Maze = () => {
+    const maze_nodes = []
+    for (let i = 0; i < 100; i++) {
+        maze_nodes.push(
+            <Grid xs={1}><Box sx={{
+                height: 25,
+                border: '1px solid #ccc',
+            }}>1</Box></Grid>
+        )
+    }
+    return (
+        <Stack className="maze" alignItems='center' mt={'2rem'}>
+            <Stack direction='row' spacing={5}>
+                <span>Visited</span>
+                <span>Wall</span>
+                <span>Start</span>
+                <span>Finish</span>
+            </Stack>
+            <Grid columns={10} container mt={'0.5rem'} sx={{'padding': '0 5rem 0 5rem'}}>
+                {maze_nodes}
+            </Grid>
+        </Stack>
+    )
+}
 
 export default Maze;
