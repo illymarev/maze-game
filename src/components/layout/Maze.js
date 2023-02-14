16ab41e1e7a9cb0d79a6be108920c289c4f18819
illymarev <illymarev@gmail.com>
2023-02-13 19:06:13 -0500

    Added notes

--- src/components/layout/Maze.js
+++ src/components/layout/Maze.js
@@ -2,8 +2,8 @@ import {Grid, Stack, Typography} from "@mui/material";
 import MazeNode from "../MazeNode";
 import MazeLegend from "../MazeLegend";
 
-const COLUMNS_NUMBER = 28;
-const ROWS_NUMBER = 12
+const COLUMNS_NUMBER = 30;
+const ROWS_NUMBER = 12;
 
 const Maze = () => {
     const maze_nodes = []
@@ -12,11 +12,17 @@ const Maze = () => {
     }
     return (
         <Stack className="maze" alignItems='center' spacing={1} marginY={'1rem'}>
-            <Typography variant="h4" component="h2">
-                Game In Progress
-            </Typography>
+            <Stack marginBottom={'0.5rem'}>
+                <Typography variant="h4" component="h2">
+                    Ready To Start
+                </Typography>
+                <Typography variant='body2' component='h4' fontWeight={300}>
+                    click the "solve" button or start solving yourself!
+                </Typography>
+            </Stack>
+
             <MazeLegend/>
-            <Grid columns={28} container width={{xs: '80%', md: '65%', lg: '60%'}}>
+            <Grid columns={COLUMNS_NUMBER} container width={{xs: '80%', md: '65%', lg: '60%'}}>
                 {maze_nodes}
             </Grid>
         </Stack>
