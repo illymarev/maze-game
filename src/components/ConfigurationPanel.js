1114302eccbb5509c9ceff1264f5a84600e55eef
illymarev <illymarev@gmail.com>
2023-05-03 19:25:18 -0400

    Slightly adjusted the maze size

--- src/components/ConfigurationPanel.js
+++ src/components/ConfigurationPanel.js
@@ -83,13 +83,23 @@ const ConfigurationPanel = observer(({gameStore, config}) => {
                                 <Button onClick={() => onGenerationButtonClick()}
                                         disabled={config.gameState.id === 4}
                                         variant="contained" color={generationButtonColor} size='large'
-                                        sx={{'border-radius': '1.25rem', width: '90%', height: {md: '50px', xxl: '60px'}, fontSize: {xl: '1.15rem', xxl: '1.25rem'}}}>
+                                        sx={{
+                                            'border-radius': '1.25rem',
+                                            width: '90%',
+                                            height: {md: '50px', xxl: '60px'},
+                                            fontSize: {xl: '1.15rem', xxl: '1.25rem'}
+                                        }}>
                                     {generationButtonText}
                                 </Button>
                                 <Button onClick={() => onSolvingButtonClick()}
                                         disabled={[0, 1].includes(config.gameState.id)}
                                         variant="contained" color={solvingButtonColor} size='large'
-                                        sx={{'border-radius': '1.25rem', width: '90%', height: {md: '50px', xxl: '60px'}, fontSize: {xl: '1.15rem', xxl: '1.25rem'}}}>
+                                        sx={{
+                                            'border-radius': '1.25rem',
+                                            width: '90%',
+                                            height: {md: '50px', xxl: '60px'},
+                                            fontSize: {xl: '1.15rem', xxl: '1.25rem'}
+                                        }}>
                                     {solvingButtonText}
                                 </Button>
                             </Stack>
@@ -137,73 +147,6 @@ const ConfigurationPanel = observer(({gameStore, config}) => {
 
         </Grid>
     )
-
-
-    // return (
-    //     <Stack spacing={1} alignItems='center' marginTop={'1rem'}>
-    //
-    //         <Stack alignItems='center' direction='row' spacing={2}>
-    //             <Stack width={'200px'}>
-    //                 <InputLabel id="generation_algorithm_label">Generation Algorithm</InputLabel>
-    //                 <Select
-    //                     disabled={config.visualizationInProgress}
-    //                     labelId="generation_algorithm_label"
-    //                     id="generation_algorithm_selector"
-    //                     sx={{'border-radius': '1.25rem'}}
-    //                     value={config.generationAlgorithm.id}
-    //                     onChange={e => config.setGenerationAlgorithm(e.target.value)}
-    //                 >
-    //                     {generationAlgorithmUIOptions}
-    //                 </Select>
-    //             </Stack>
-    //             <Stack spacing={2}>
-    //                 <Button onClick={() => onGenerationButtonClick()}
-    //                         disabled={config.gameState.id === 4}
-    //                         variant="contained" color={generationButtonColor} size='large'
-    //                         sx={{width: '200px', height: '50px', 'border-radius': '1.25rem'}}>
-    //                     {generationButtonText}
-    //                 </Button>
-    //                 <Button onClick={() => onSolvingButtonClick()}
-    //                         disabled={[0, 1].includes(config.gameState.id)}
-    //                         variant="contained" color={solvingButtonColor} size='large'
-    //                         sx={{width: '200px', height: '50px', 'border-radius': '1.25rem'}}>
-    //                     {solvingButtonText}
-    //                 </Button>
-    //             </Stack>
-    //             <Stack width={'200px'}>
-    //                 <InputLabel id="solving_algorithm_label">Solving Algorithm</InputLabel>
-    //                 <Select
-    //                     disabled={config.visualizationInProgress}
-    //                     labelId="solving_algorithm_label"
-    //                     id="solving_algorithm_selector"
-    //                     sx={{'border-radius': '1.25rem'}}
-    //                     value={config.solvingAlgorithm.id}
-    //                     onChange={e => config.setSolvingAlgorithm(e.target.value)}
-    //                 >
-    //                     {solvingAlgorithmUIOptions}
-    //                 </Select>
-    //             </Stack>
-    //         </Stack>
-    //
-    //         <Stack direction='column' justifyContent='center' alignItems='center'>
-    //             <InputLabel id="visualization_speed_label">Visualization Speed</InputLabel>
-    //             <Slider
-    //                 disabled={config.visualizationInProgress}
-    //                 step={null}
-    //                 marks={visualizationSpeedUIOptions}
-    //                 labelId="visualization_speed_label"
-    //                 id='visualization_speed_slider'
-    //                 value={config.visualizationSpeed.id}
-    //                 valueLabelDisplay="off"
-    //                 size='medium'
-    //                 min={0} max={4}
-    //                 sx={{width: '625px'}}
-    //                 onChange={e => config.setVisualizationDelay(e.target.value)}
-    //             />
-    //         </Stack>
-    //
-    //     </Stack>
-    // )
 })
 
 export default ConfigurationPanel;
