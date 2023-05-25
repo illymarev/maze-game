f7303e8eaabb745caba827d2b44017cea0051ece
illymarev <illymarev@gmail.com>
2023-05-25 07:57:23 -0400

    Cleaned up the Kruskal's algorithm, moved utils functions to utils.js

--- src/algorithms/generation/kruskalsAlgorithm.js
+++ src/algorithms/generation/kruskalsAlgorithm.js
@@ -1,35 +1,4 @@
-import {determineDirectionAndMarkPath} from "./utils";
-
-
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-
-const getAllPossibleEdges = maze => {
-    const [rows, columns] = [maze.length, maze[0].length]
-
-    const edges = []
-    for (let row of maze) {
-        // columns -1 is needed in order to connect the second last one with the last one
-        for (let columnNumber = 0; columnNumber < columns - 1; columnNumber++) {
-            const edge = [row[columnNumber], row[columnNumber + 1]]
-            edges.push(edge)
-        }
-    }
-
-    for (let columnNumber = 0; columnNumber < columns; columnNumber++) {
-        for (let rowNumber = 0; rowNumber < rows - 1; rowNumber++) {
-            const edge = [maze[rowNumber][columnNumber], maze[rowNumber + 1][columnNumber]]
-            edges.push(edge)
-        }
-    }
-
-    return edges
-}
-
+import {determineDirectionAndMarkPath, shuffleArray, getAllPossibleEdges} from "./utils";
 
 const find = node => {
     if (node.parent !== node) {
@@ -55,13 +24,7 @@ const union = (node1, node2, actionsToVisualize) => {
     determineDirectionAndMarkPath(node1, node2, actionsToVisualize)
 }
 
-/**
- * This is a modified version of the kruskal's algorithm. Since the graph is unweighted, edges are picked in random
- * order
- * @param visualizeCurrent whether the "current" should be highlighted during the visualization or not
- * @param maze
- * @returns {{newMaze, actionsToVisualize: *[]}}
- */
+// Modified version of the kruskal's algorithm used to generate graphs instead of finding the MST
 const kruskalsAlgorithm = (visualizeCurrent, maze) => {
     const allPossibleEdges = getAllPossibleEdges(maze)
     shuffleArray(allPossibleEdges)
@@ -73,7 +36,6 @@ const kruskalsAlgorithm = (visualizeCurrent, maze) => {
             node.rank = 1
         }
     }
-
 
     for (const edge of allPossibleEdges) {
         const [node1, node2] = edge
@@ -92,7 +54,6 @@ const kruskalsAlgorithm = (visualizeCurrent, maze) => {
                 payload: [{row: node1.row, column: node1.column}, {row: node2.row, column: node2.column}]
             })
         }
-
     }
 
     return {newMaze: maze, actionsToVisualize: actionsToVisualize}
--- src/algorithms/generation/utils.js
+++ src/algorithms/generation/utils.js
@@ -18,3 +18,32 @@ export const determineDirectionAndMarkPath = (startNode, endNode, actionsToVisua
         ]
     })
 }
+
+export const shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+export const getAllPossibleEdges = maze => {
+    const [rows, columns] = [maze.length, maze[0].length]
+
+    const edges = []
+    for (let row of maze) {
+        // columns -1 is needed in order to connect the second last one with the last one
+        for (let columnNumber = 0; columnNumber < columns - 1; columnNumber++) {
+            const edge = [row[columnNumber], row[columnNumber + 1]]
+            edges.push(edge)
+        }
+    }
+
+    for (let columnNumber = 0; columnNumber < columns; columnNumber++) {
+        for (let rowNumber = 0; rowNumber < rows - 1; rowNumber++) {
+            const edge = [maze[rowNumber][columnNumber], maze[rowNumber + 1][columnNumber]]
+            edges.push(edge)
+        }
+    }
+
+    return edges
+}
